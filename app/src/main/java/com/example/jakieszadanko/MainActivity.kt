75dbd95bf50bd75100a6a77d3a845fb6476f834e
sztoslol <kubuszeski05@wp.pl>
2022-12-06 10:56:31 +0100

    Dodano:    -Przy starcie program ustala max i min date

--- app/src/main/java/com/example/jakieszadanko/MainActivity.kt
+++ app/src/main/java/com/example/jakieszadanko/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jakieszadanko
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.format.DateUtils
@@ -7,10 +8,17 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.get
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,9 +26,11 @@ class MainActivity : AppCompatActivity() {
         // Przypisanie funkcjonalnych elementow UI do zmiennych
         var button_odjazd = findViewById<Button>(R.id.button_data_wyjazdu)
         var button_powrot = findViewById<Button>(R.id.button_data_powrotu)
-        var kalendar_view = findViewById<CalendarView>(R.id.calendarView_main)
+        var kalendarz = findViewById<CalendarView>(R.id.calendarView_main)
         var text_wynik = findViewById<TextView>(R.id.textView_wynik)
 
-
+        var kalendarz_zaznaczone = kalendarz.date // zaznaczona data
+        kalendarz.minDate = System.currentTimeMillis() // ustalenie minimalnej daty
+        kalendarz.maxDate = System.currentTimeMillis() + (kalendarz.maxDate - System.currentTimeMillis())// ustalanie maxymalnej daty
     }
 }
