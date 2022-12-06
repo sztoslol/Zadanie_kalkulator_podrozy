46334ac6b25b0a549b640bb3205f7ca2c1cf1057
sztoslol <kubuszeski05@wp.pl>
2022-12-06 09:27:46 +0100

    Przypisanie funkcjonalnych elementow UI do zmiennych

--- app/src/main/java/com/example/jakieszadanko/MainActivity.kt
+++ app/src/main/java/com/example/jakieszadanko/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.jakieszadanko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Przypisanie funkcjonalnych elementow UI do zmiennych
+        var button_odjazd = findViewById<Button>(R.id.button_data_wyjazdu)
+        var button_powrot = findViewById<Button>(R.id.button_data_powrotu)
+        var kalendar_view = findViewById<CalendarView>(R.id.calendarView_main)
+        var text_wynik = findViewById<TextView>(R.id.textView_wynik)
     }
 }
